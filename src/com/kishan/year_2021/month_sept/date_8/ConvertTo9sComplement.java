4893f2487ed4db74b20d63e74a626f820f8c885a
Kishan Mevada <mevada.kishan@gmail.com>
2021-09-08 09:15:22 +0300

    9's complement

---
+++ src/com/kishan/year_2021/month_sept/date_8/ConvertTo9sComplement.java
@@ -0,0 +1,24 @@
+package com.kishan.year_2021.month_sept.date_8;
+
+import java.util.Arrays;
+
+/**
+ * 	9’s complement of a decimal number
+ * 	We traverse digits of number and subtract every digit from 9.
+ */
+public class ConvertTo9sComplement {
+	static void convert(String number){
+		char[] numChar = number.toCharArray();
+		for(int i = 0; i < numChar.length; i++) {
+			if(numChar[i] != '.'){
+				numChar[i] = (char)((int)('9') - (int)(numChar[i]) + (int)('0'));
+			}
+
+		}
+		System.out.println("9's Complement of " + number +" -> " + String.valueOf(numChar));
+	}
+	public static void main(String[] args) {
+		convert("22");
+		convert("125.55");
+	}
+}
