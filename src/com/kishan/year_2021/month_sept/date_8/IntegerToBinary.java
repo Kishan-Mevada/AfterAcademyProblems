be5288b6620593c5892b91b6799777477b9b55c9
Kishan Mevada <mevada.kishan@gmail.com>
2021-09-08 09:20:48 +0300

    integer to binary recursive approach added

--- src/com/kishan/year_2021/month_sept/date_8/IntegerToBinary.java
+++ src/com/kishan/year_2021/month_sept/date_8/IntegerToBinary.java
@@ -5,7 +5,7 @@ package com.kishan.year_2021.month_sept.date_8;
  * 	8 bit representation
  */
 public class IntegerToBinary {
-
+	private static final int BASE = 8;
 	static String convertToBinary(int num){
 		StringBuilder sb = new StringBuilder();
 
@@ -13,13 +13,30 @@ public class IntegerToBinary {
 			sb.insert(0,num % 2);
 			num = num / 2;
 		}
-		for(int i = sb.length(); i < 8; i++) {
+		for(int i = sb.length(); i < BASE; i++) {
 			sb.insert(0,0);
 		}
 		return sb.toString();
 	}
+
+	static String convertBinaryRec(int num){
+		StringBuilder sb = new StringBuilder(convertRec(num));
+		for(int i = sb.length(); i < BASE; i++) {
+			sb.insert(0,0);
+		}
+		return sb.toString();
+	}
+
+	private static String convertRec(int num){
+		if(num == 0){
+			return "0";
+		}
+		return convertRec(num/2) + (num % 2);
+	}
+
 	public static void main(String[] args) {
 		int num = 11;
 		System.out.println("Integer to Binary -> " + convertToBinary(num));
+		System.out.println("Integer to Binary -> " + convertBinaryRec(num));
 	}
 }
